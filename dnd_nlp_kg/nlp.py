c386171c75e552fc5c59ccaeb2bdc18c6c1bc701
Sooty <7614538+SootyOwl@users.noreply.github.com>
2022-03-17 00:42:22 +0000

    flake8black

--- dnd_nlp_kg/nlp.py
+++ dnd_nlp_kg/nlp.py
@@ -1,17 +1,16 @@
 """Use SpaCy to extract relationship triples from input text."""
 
-from collections import namedtuple
 from dataclasses import dataclass
 from typing import List
 import spacy
 from spacy.matcher import Matcher
-import coreferee
 
-nlp = spacy.load('en_core_web_sm')
-nlp.add_pipe('sentencizer')
-nlp.add_pipe('coreferee')
-nlp.add_pipe('merge_noun_chunks')
-nlp.add_pipe('merge_entities')
+nlp = spacy.load("en_core_web_sm")
+nlp.add_pipe("sentencizer")
+nlp.add_pipe("coreferee")
+nlp.add_pipe("merge_noun_chunks")
+nlp.add_pipe("merge_entities")
+
 
 @dataclass
 class Triple:
@@ -26,16 +25,18 @@ class Triple:
 def read(filename) -> str:
     """Read example data from a given filename."""
     import os
+
     dirname = os.path.dirname(__file__)
     filename = os.path.join(dirname, filename)
-    with open(filename, 'r') as fin:
-        return '. '.join([line.strip() for line in fin.readlines()])
+    with open(filename, "r") as fin:
+        return ". ".join([line.strip() for line in fin.readlines()])
+
 
 def subtree_matcher(sent):
     subjpass = 0
 
     for tok in sent:
-        # find dependency tag that contains the text "subjpass"    
+        # find dependency tag that contains the text "subjpass"
         if not tok.dep_.find("subjpass") == -1:
             subjpass = 1
             break
@@ -45,29 +46,30 @@ def subtree_matcher(sent):
     # TODO: use a matcher, define some patterns
     # if subjpass == 1 then sentence is passive
     if subjpass == 1:
-        for i,tok in enumerate(sent):
-            if tok.dep_.find("subjpass") == True:
+        for i, tok in enumerate(sent):
+            if tok.dep_.find("subjpass") is True:
                 y = tok
 
-            if tok.dep_.endswith("obj") == True:
+            if tok.dep_.endswith("obj") is True:
                 x = tok
 
             if x and y:
                 break
-    
+
     # if subjpass == 0 then sentence is not passive
     else:
-        for i,tok in enumerate(sent):
-            if tok.dep_.endswith("subj") == True:
+        for i, tok in enumerate(sent):
+            if tok.dep_.endswith("subj") is True:
                 x = tok
 
-            if tok.dep_.endswith("obj") == True:
+            if tok.dep_.endswith("obj") is True:
                 y = tok
 
             if x and y:
                 break
 
-    return x,y
+    return x, y
+
 
 def get_relation(sent):
     """
@@ -76,19 +78,21 @@ def get_relation(sent):
     # Matcher class object
     matcher = Matcher(nlp.vocab)
 
-    #define the pattern
-    pattern = [{'DEP':'ROOT'},
-               {'DEP': 'det', 'OP': "?"},
-               {'DEP': 'attr', 'OP': "?"},
-               {'DEP':'prep','OP':"?"},
-            #    {'DEP':'agent','OP':"?"},
-               {'POS':'ADJ','OP':"?"}
-               ]
+    # define the pattern
+    pattern = [
+        {"DEP": "ROOT"},
+        {"DEP": "det", "OP": "?"},
+        {"DEP": "attr", "OP": "?"},
+        {"DEP": "prep", "OP": "?"},
+        #    {'DEP':'agent','OP':"?"},
+        {"POS": "ADJ", "OP": "?"},
+    ]
 
     matcher.add("relation", [pattern])
 
     matches = matcher(sent, as_spans=True, with_alignments=True)
     return matches[-1]
+
 
 def logic(input: str) -> List[Triple]:
     """Process natural language text to extract relationship triples from every sentence."""
@@ -101,19 +105,21 @@ def logic(input: str) -> List[Triple]:
         for sub in (subj, *subj.conjuncts):
             triples.append(Triple(sub.text, pred.text, obj.text))
     return triples
-    
+
 
 def write(triples: List[Triple]) -> None:
     """Output the triples"""
     # just print them
-    triple_output: str = '\n'.join([str(t) for t in triples])
+    triple_output: str = "\n".join([str(t) for t in triples])
     print(triple_output)
+
 
 def main():
     """Run the RLW."""
-    input = read('./data/tarakona.txt')
+    input = read("./data/tarakona.txt")
     triple_list = logic(input)
     write(triple_list)
+
 
 if __name__ == "__main__":
     main()
